c5496f9a85b00c99efd275cb9a32f0f050ddb724
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-06-01 10:11:52 -0400

    handlesubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
+import data from "./data";
+import { nanoid } from "nanoid";
 
 const App = () => {
   const [count, setCount] = useState(1);
   const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+  };
   return (
     <section className="section-center">
       <h4>Tired of boring lorem ipsum?</h4>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">paragraphs:</label>
         <input
           type="number"
@@ -22,6 +29,11 @@ const App = () => {
           generate
         </button>
       </form>
+      <article className="lorem-text">
+        {text.map((item) => {
+          return <p key={nanoid()}>{item}</p>;
+        })}
+      </article>
     </section>
   );
 };
